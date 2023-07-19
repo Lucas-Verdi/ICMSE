b5c88429c683a5432d050aea934a3508d874f3f6
Lucas <lucas.a.verdi@gmail.com>
2023-07-19 15:48:54 -0300

    Update

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ results = []
 numeronota = []
 ipinota = []
 dataemi = []
+razaosocial = []
+valornota = []
 
 def start():
     a = Th(1)
@@ -30,10 +32,16 @@ def ler_icms(xml_file):
     numero = root.xpath(".//nfe:nNF/text()", namespaces=namespaces)
     ipi = root.xpath(".//nfe:ICMSTot/nfe:vIPI/text()", namespaces=namespaces)
     data = root.xpath(".//nfe:dhEmi/text()", namespaces=namespaces)
+    razao = root.xpath(".//nfe:emit/nfe:xNome/text()", namespaces=namespaces)
+    valortot = root.xpath(".//nfe:ICMSTot/nfe:vNF/text()", namespaces=namespaces)
+
+
 
     numeronota.append(numero)
     ipinota.append(ipi)
     dataemi.append(data)
+    razaosocial.append(razao)
+    valornota.append(valortot)
 
     return results.append(final)
 
@@ -60,13 +68,17 @@ class Th(Thread):
         sheet = workbook.sheets.active
         sheet.range('A1').value = "DATA"
         sheet.range('B1').value = "NOTA"
-        sheet.range('C1').value = "ICMS"
-        sheet.range('D1').value = "IPI"
+        sheet.range('C1').value = "RAZAO SOCIAL"
+        sheet.range('D1').value = "ICMS"
+        sheet.range('E1').value = "IPI"
+        sheet.range('F1').value = "TOTAL NF"
 
         sheet.range('A2').value = dataemi
         sheet.range('B2').value = numeronota
-        sheet.range('C2').value = results
-        sheet.range('D2').value = ipinota
+        sheet.range('C2').value = razaosocial
+        sheet.range('D2').value = results
+        sheet.range('E2').value = ipinota
+        sheet.range('F2').value = valornota
 
         arquivos.clear()
         results.clear()
